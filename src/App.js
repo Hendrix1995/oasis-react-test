82128695298b260ebad3a830b03e54d22477cd4b
Hendrix1995 <hgud55@naver.com>
2022-02-08 17:56:13 +0900

    FIX: Home albums 데이터 Promise all로 서버요청 변경

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import Header from "./components/Header.js";
 import Home from "./pages/Home.js";
 
 function App() {
-    const [album_10, setAlbum_10] = useState([]);
-    const [album_20, setAlbum_20] = useState([]);
+    const [albums, setAlbums] = useState([]);
 
-    const getAlbum = () => {
-        axios
-            .get(`https://jsonplaceholder.typicode.com/albums/10/photos`, { withCredentials: true })
-            .then((res) => setAlbum_10(res.data))
-            .catch((err) => console.log("10번 엘범 데이터 수신 애러"));
-        axios
-            .get(`https://jsonplaceholder.typicode.com/albums/20/photos`, { withCredentials: true })
-            .then((res) => setAlbum_20(res.data))
-            .catch((err) => console.log("20번 엘범 데이터 수신 애러"));
+    const getAlbum = (ids) => {
+        return Promise.all(
+            ids.map((id) =>
+                axios
+                    .get(`https://jsonplaceholder.typicode.com/albums/${id}/photos`, { withCredentials: true })
+                    .then((res) => res.data)
+                    .catch((err) => console.log(err))
+            )
+        );
     };
 
     useEffect(() => {
-        getAlbum();
+        getAlbum([10, 20]).then((album) => setAlbums(album));
     }, []);
 
     return (
         <>
             <Header />
-            <Home album_10={album_10} album_20={album_20} />
+            <Home albums={albums} />
         </>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-function Home({ album_10, album_20 }) {
+function Home({ albums }) {
     return (
         <section className="home">
             <div className="album-continuer">
-                {album_10.map((el) => {
+                {/* {album_10.map((el) => {
                     return el.id % 2 === 0 ? <img key={el.id} className="album-thumbnail" src={el.thumbnailUrl} alt={el.id} /> : null;
                 })}
                 {album_20.map((el) => {
                     return el.id % 2 === 0 ? <img key={el.id} className="album-thumbnail" src={el.thumbnailUrl} alt={el.id} /> : null;
-                })}
+                })} */}
             </div>
         </section>
     );
